2ed8aef255726b507ff67508939e7badc9e9fe4f
Saewran <Gadzillos@gmail.com>
2021-11-02 12:20:09 +0300

    added default naming for resources
    ain

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -6,10 +6,12 @@ variable "location" {
 
 variable "resource_group_name" {
   type        = string
+  default     = "WildfyGroup"
   description = "resource group name"
 }
 
 variable "storage_account_name" {
   type        = string
+  default     = "WildfyStorage"
   description = "storage account name"
 }
