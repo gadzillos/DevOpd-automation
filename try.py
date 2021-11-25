b6ad66e939275038d580cbeebea13f8a13d248e3
AirtimeBalandin <balandin-tema@mail.ru>
2021-11-25 21:21:47 +0000

    Update try.py Working code and execution

--- try.py
+++ try.py
@@ -1,20 +1,33 @@
 import os
-import subprocess
 
-#installing repo
-subprocess.run("cd",  "/etc")
-subprocess.run("mkdir", "devop")
-subprocess.run("cd", "/devop")
-subprocess.run("git", "clone", "https://gitlab.com/devops_netcraker/repo.git")
-)
-
+os.system("sudo yum update")
+os.system("sudo yum install dnf")
+os.system("sudo yum install wget unzip")
 
 #Azure installation
-subprocess.run("rpm --import https://packages.microsoft.com/keys/microsoft.asc")
-subprocess.run("echo", "-e", "[azure-cli]", "name=Azure CLI", "baseurl=https://packages.microsoft.com/yumrepos/azure-cli" ,"enabled=1", "gpgcheck=1", "gpgkey=https://packages.microsoft.com/keys/microsoft.asc", "sudo",  "tee", "/etc/yum.repos.d/azure-cli.repo")
-subprocess.run("sudo", "dnf", "install",  "azure-cli")
+os.system("sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc")
+os.system("""echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo""")
+os.system("sudo dnf install azure-cli")
+os.system("az login")
+
+answer ="0"
+while answer != "y":
+	os.system("echo Continue? Press y when logged in.")
+	answer = raw_input()
 
 #terraform launch
-os.system("echo terraform init")
-os.system("echo terraform validate")
-os.system("echo terraform apply")
+os.system("wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip")
+os.system("sudo unzip ./terraform_1.0.11_linux_amd64.zip -d /usr/local/bin/")
+os.system("terraform -v")
+
+
+import os
+os.chdir("/home/centos/Downloads/repo-python/Terraform")
+os.system("terraform init")
+os.system("terraform validate")
+os.system("terraform apply")
