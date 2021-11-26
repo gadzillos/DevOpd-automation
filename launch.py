b705cc8cdc925b5229c561b960db62dd90a4bb26
Saewran <Gadzillos@gmail.com>
2021-11-26 22:41:47 +0300

    fixed system pathing,  added -y options, added azuzre auth dialog, login not implemented

--- launch.py
+++ launch.py
@@ -2,12 +2,14 @@ import os
 
 script_path = os.getcwd()
 
+script_path += "/Terraform"
+
 #os.system("sudo yum -y update")
 os.system("sudo yum -y install dnf")
 os.system("sudo yum -y install wget unzip")
 
 #Download and unzip terraform to bin
-os.chdir("~/")
+os.chdir(os.path.expanduser('~'))
 os.system("wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip")
 os.system("sudo unzip ./terraform_1.0.11_linux_amd64.zip -d /usr/local/bin/")
 os.system("terraform -v")
@@ -22,14 +24,26 @@ gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo""")
 os.system("sudo dnf -y install azure-cli")
 
-#Azure login
-os.system("az login") #az login -u <username> -p <password>
+os.system("echo '\n~~~~~~~~~~\nChoose Azure login option:\n~~~~~~~~~~\n'")
+os.system("echo '1: login -u <username> -p <password>\n'")
+os.system("echo '2: service principal login <app-id> <password-or-cert> <tenant>\n'")
 
 answer ="0"
-while answer != "y":
-	os.system("az login")
-	os.system("echo Continue? Press y when logged in.")
-	answer = raw_input()
+while not ((answer == "1") or (answer == "2")):
+    os.system("\n")
+    answer = raw_input()
+
+if answer == '1':
+	print()
+	#az login -u <username> -p <password>
+else:
+	print()
+	# az login --service-principal -u <app-id> -p <password-or-cert> --tenant <tenant>
+
+
+#Azure login
+os.system("az login") 
+
 
 #terraform launch
 import os
