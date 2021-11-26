74fbae6e60cbfaff0161b91a97bd7b87bcc60088
Saewran <Gadzillos@gmail.com>
2021-11-26 16:24:01 +0300

    code restructure + path fix

--- launch.py
+++ launch.py
@@ -1,8 +1,16 @@
 import os
 
-os.system("sudo yum -y update")
+script_path = os.getcwd()
+
+#os.system("sudo yum -y update")
 os.system("sudo yum -y install dnf")
 os.system("sudo yum -y install wget unzip")
+
+#Download and unzip terraform to bin
+os.chdir("~/")
+os.system("wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip")
+os.system("sudo unzip ./terraform_1.0.11_linux_amd64.zip -d /usr/local/bin/")
+os.system("terraform -v")
 
 #Azure installation
 os.system("sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc")
@@ -12,7 +20,9 @@ baseurl=https://packages.microsoft.com/yumrepos/azure-cli
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo""")
-os.system("sudo dnf install azure-cli")
+os.system("sudo dnf -y install azure-cli")
+
+#Azure login
 os.system("az login") #az login -u <username> -p <password>
 
 answer ="0"
@@ -22,13 +32,8 @@ while answer != "y":
 	answer = raw_input()
 
 #terraform launch
-os.system("wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip")
-os.system("sudo unzip ./terraform_1.0.11_linux_amd64.zip -d /usr/local/bin/")
-os.system("terraform -v")
-
-
 import os
-os.chdir("/home/centos/Downloads/repo-python/Terraform")
+os.chdir(script_path)
 os.system("terraform init")
 os.system("terraform validate")
 os.system("terraform apply")
