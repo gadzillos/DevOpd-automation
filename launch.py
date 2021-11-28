3667a440dc220e74948f3c834b67f190b24ef513
AirtimeBalandin <balandin-tema@mail.ru>
2021-11-28 12:30:57 +0000

    Update launch.py with automatic login options

--- launch.py
+++ launch.py
@@ -27,23 +27,27 @@ os.system("sudo dnf -y install azure-cli")
 os.system("echo '\n~~~~~~~~~~\nChoose Azure login option:\n~~~~~~~~~~\n'")
 os.system("echo '1: login -u <username> -p <password>\n'")
 os.system("echo '2: service principal login <app-id> <password-or-cert> <tenant>\n'")
+os.system("echo '3: login using browser\n'")
 
 answer ="0"
-while not ((answer == "1") or (answer == "2")):
+while not ((answer == "1") or (answer == "2") or (answer == "3")):
     os.system("\n")
-    answer = raw_input()
+    answer = raw_input("Type in option number: ")
 
 if answer == '1':
-    print()
-	#az login -u <username> -p <password>
+    login = raw_input("Type in your login: ")
+    password = raw_input("Type in your password: ")
+    os.system("az login -u " + login + " -p "+ password)
+elif answer == '3':
+    os.system("az login")
 else:
-    print()
-	# az login --service-principal -u <app-id> -p <password-or-cert> --tenant <tenant>
-
+    appID = raw_input("Type in your app-id: ")
+    password = raw_input("Type in your password/cert: ")
+    tenant = raw_input("Type in tenant: ")
+    os.system("az login --service-principal -u "+ appID +" -p "+ password +" --tenant "+ tenant)
 
 #Azure login
 os.system("az login") 
-
 
 #terraform launch
 import os
