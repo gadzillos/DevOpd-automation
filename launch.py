c966948f69173a3d05679bc084893ae931c74df6
Saewran <Gadzillos@gmail.com>
2021-12-11 21:00:32 +0300

    added terminal_message; added known_hosts creation if none present

--- launch.py
+++ launch.py
@@ -3,7 +3,21 @@ import subprocess
 import argparse
 import codecs
 import json
+import shutil
 
+
+def terminal_message(message):
+    cyan = "\033[0;36m"
+    no_color = "\033[0m"
+    (columns, lines) = shutil.get_terminal_size()
+    fill_line = ""
+    for i in range(lines):
+        fill_line += "~"
+    os.system(f"echo && echo -e '{cyan}{fill_line}'' && echo")
+    os.system(f"echo -e '{cyan}{message}'")
+    os.system(f"echo && echo '{cyan}{fill_line}{no_color}' && echo")
+    
+    
 #parsing file with login to Azure
 parser = argparse.ArgumentParser()
 parser.add_argument('-p','--path', help='implement path to login file')
@@ -32,6 +46,7 @@ enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo""")
 os.system("sudo dnf -y install azure-cli")
+terminal_message("Installed azure-cli")
 
 
 def login_and_terraform_apply(subscription_id, app_id, password_value, tenant_value):
@@ -43,7 +58,7 @@ def login_and_terraform_apply(subscription_id, app_id, password_value, tenant_va
                     f"az login --service-principal -u '{app_id}' -p '{password_value}' --tenant '{tenant_value}' &&" +
                     f"terraform init && terraform validate && terraform apply -auto-approve && echo done!")
     process = subprocess.run(bash_command, shell = True)
-
+    
 
 def inventory_build():
     os.chdir(original_path + "/Ansible")
@@ -73,28 +88,32 @@ password = data['password']
 tenant = data['tenant']
 subscription_ID = data['subscriptionId']
 login_and_terraform_apply(subscription_ID, app_ID, password, tenant)
+terminal_message("Terraform applied")
     
 # Ansible inventory build
 inventory_build()
+terminal_message("Inventory builded")
 
 # Add VM2 to known_hosts for ssh connection
+os.system("touch known_hosts")
 os.system(f"ssh-keyscan -H {ip2} >> ~/.ssh/known_hosts")
 
 #Ansible installation
-os.system("echo <-------- Updating system -------->")
+terminal_message("Updating system")
 os.system("sudo yum update -y")
 os.system("sudo yum install -y python3-pip")
 os.system("sudo pip3 install --upgrade pip")
 
-os.system("echo <-------- Installing Ansible -------->")
+terminal_message("Installing Ansible")
 os.system("pip3 install 'ansible==2.9.17'")
 
-os.system("echo <-------- Installing git. Clone -------->")
+terminal_message("Installing git. Clone")
 os.system("ansible --version")
 os.system("sudo yum -y install git")
 os.system("git --version")
 
 # send tar archive to VM2
+terminal_message("sending tar file to VM")
 os.chdir(original_path)
 os.system(f"tar -cf repo.tar . &&" + 
           f"rsync --rsh='ssh -i sshVM2.pem' repo.tar azureuser@{ip2}:~")
