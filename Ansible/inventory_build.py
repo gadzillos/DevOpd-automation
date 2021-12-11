ae793d3275e3f2bd135e89dab4e664a07554eceb
Saewran <Gadzillos@gmail.com>
2021-12-11 22:23:24 +0300

    fixed ssh

--- Ansible/inventory_build.py
+++ Ansible/inventory_build.py
@@ -9,11 +9,9 @@ with open('../VM3publicip.txt') as f:
 with open('../VM1privateip.txt') as f:
     ip1 = f.read()
     
-inventory = (f"[jenkins_node]\n" + f"{ip2} ansible_ssh_user=azureuser ansible_ssh_private_key=/sshVM2.pem\n\n" +
-             f"[docker_node]\n" + f"{ip1} ansible_ssh_user=azureuser ansible_ssh_private_key=/sshVM1.pem\n\n" +
-             f"[wildfly_node]\n" + f"{ip3} ansible_ssh_user=azureuser ansible_ssh_private_key=/sshVM3.pem\n\n")
+inventory = (f"[jenkins_node]\n" + f"{ip2} ansible_user=azureuser ansible_ssh_private_key_file=../sshVM2.pem\n\n" +
+             f"[docker_node]\n" + f"{ip1} ansible_user=azureuser ansible_ssh_private_key_file=../sshVM1.pem\n\n" +
+             f"[wildfly_node]\n" + f"{ip3} ansible_user=azureuser ansible_ssh_private_key_file=../sshVM3.pem\n\n")
 
 with open('inventory', 'w') as f:
     f.writelines(inventory)
-#os.system(f"export Inventory={inventory}")
-#os.system(f"sudo echo {inventory} > inventory")
