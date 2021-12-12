cba1638db06c65dc226619088ec191f6df6a2c2a
Saewran <Gadzillos@gmail.com>
2021-12-12 03:21:21 +0300

    added touch for known_hosts

--- add_known_hosts.py
+++ add_known_hosts.py
@@ -6,6 +6,7 @@ with open('VM3publicip.txt') as f:
     ip3 = f.read()    
 with open('VM1privateip.txt') as f:
     ip1 = f.read()
-    
+
+os.system("sudo touch ~/.ssh/known_hosts")
 os.system(f"ssh-keyscan -H {ip1} >> ~/.ssh/known_hosts")
 os.system(f"ssh-keyscan -H {ip3} >> ~/.ssh/known_hosts")
