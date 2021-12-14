99e1a6fe32e4c85d26a186a58f4b4cd7131c5648
Saewran <Gadzillos@gmail.com>
2021-12-14 17:21:06 +0300

    removed now unused playbook start

--- launch.py
+++ launch.py
@@ -111,8 +111,5 @@ os.system(f"tar -cf repo.tar . &&" +
           f"rsync --rsh='ssh -i sshVM2.pem' repo.tar azureuser@{ip2}:~ && echo 'rsync done'")
 
 # Ansible playbook start
-terminal_message("Starting ansilbe kenkins_node_preparation.yml on VM2")
+terminal_message("Starting ansilbe jenkins_node_preparation.yml on VM2")
 os.chdir(original_path + "/Ansible")
-os.system("ansible-playbook -v -i inventory jenkins_node_preparation.yml")
-terminal_message("Starting ansilbe kenkins_job.yml on VM2")
-os.system("ansible-playbook -v -i inventory jenkins_job.yml")
