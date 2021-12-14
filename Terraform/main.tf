80ab45f7909dc6a43766da013935c5d831153cd2
Saewran <Gadzillos@gmail.com>
2021-12-14 19:53:30 +0300

    open 8080 port for jenkins in NSG

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -85,6 +85,18 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "jenkins"
+    priority                   = 1011
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Any"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     environment = local.environment
   }
