95cd9a3fdf133f44c7e05d4cbc0434f3e0a8049b
Saewran <Gadzillos@gmail.com>
2021-12-14 20:36:53 +0300

    fixed protocol

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -90,7 +90,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     priority                   = 1011
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Any"
+    protocol                   = "*"
     source_port_range          = "*"
     destination_port_range     = "8080"
     source_address_prefix      = "*"
