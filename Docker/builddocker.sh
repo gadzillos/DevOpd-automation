b137140a23bab576f3b0d99600401488408d40e1
Saewran <Gadzillos@gmail.com>
2021-12-17 02:01:13 +0300

    shell script fix

--- Docker/builddocker.sh
+++ Docker/builddocker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Starting shell script"
-sudo cd /opt/repo/Docker
-echo "build the docker image"
-sudo docker build -t docker_psql
+echo "building the docker image"
+sudo docker build . -t docker_psql
