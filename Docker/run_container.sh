4fc0f6fa66e289250af71a290a6ea57e21f88fa3
Saewran <Gadzillos@gmail.com>
2021-12-20 23:50:01 +0300

    docker working now; added right commands

--- Docker/run_container.sh
+++ Docker/run_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # create persistent storage directory
 mkdir -p $HOME/docker/volumes/postgres
-docker run --rm --name docker_db -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/pgsql/14 docker_psql
+docker run --rm --name docker_db -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data docker_psql
--- Docker/startup.sh
+++ Docker/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 systemctl enable postgresql-14.service
 systemctl start postgresql-14.service
--u postgres bash << EOF
+sudo -u postgres bash << EOF
 echo "In"
 psql --command "CREATE USER docker WITH SUPERUSER PASSWORD 'docker';" &&
 createdb -O docker docker
