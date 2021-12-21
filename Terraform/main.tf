3cab48312cf547afa7d735050b59346c6d40a58e
Saewran <Gadzillos@gmail.com>
2021-12-22 00:50:29 +0300

    Standard_D2s_v3

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -272,7 +272,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm_1" {
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.Nic1.id]
-  size                  = "Standard_A2_v2"
+  size                  = "Standard_D2s_v3"
 
   os_disk {
     name                 = "VM1disk"
@@ -318,7 +318,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm_2" {
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.Nic2.id]
-  size                  = "Standard_A2_v2"
+  size                  = "Standard_D2s_v3"
 
   os_disk {
     name                 = "VM2disk"
@@ -357,7 +357,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm_3" {
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.Nic3.id]
-  size                  = "Standard_A2_v2"
+  size                  = "Standard_D2s_v3"
 
   os_disk {
     name                 = "VM3disk"
